ec7c6dc1315b8b9e91f810686cc26bdb746e2c33
unknown <destyrahmadany05@gmail.com>
2023-06-02 02:28:42 -0700

    Buat Halaman Search

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/search")
 def home():
-    return render_template('home.html')
+    return render_template('search.html')
 
 
 if __name__ == '__main__':
